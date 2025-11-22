056299a5aa971f88482b0be189a37f220fe0ec09
Sanjay Jangid <sanjay50986@gmail.com>
2025-11-22 11:30:28 +0530

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "./Hero.css"
+import headphone from "../../assets/vibrating-headphone.png"
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='hero-section'>
+      <div>
+        <h2>100 Thousand Songs, ad-free</h2>
+        <h2>Over thousands podcast episodes</h2>
+      </div>
+
+    <img className='headphone-icon' src={headphone} alt="headphone" />
+    </header>
   )
 }
 
